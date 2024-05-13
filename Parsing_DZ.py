611251c00463de1c83cfe220609169d020eea069
Тимур Калимуллин <166409162+TimKubrik@users.noreply.github.com>
2024-05-13 07:06:09 +0300

    Add files via upload

--- Parsing_DZ.py
+++ Parsing_DZ.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
-
+import time
 def write_cmc_top():
     driver = webdriver.Chrome()  # Инициализация драйвера Chrome
     driver.get('https://coinmarketcap.com/')  # Открытие страницы
+
+    # Прокрутка и прогрузка страницы
+    for i in range(15):  # Прокрутить страницу 13 раз
+        driver.execute_script('window.scrollBy(0, window.innerHeight)')
+        time.sleep(5)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     list_name = soup.find_all('p', {'class': 'sc-4984dd93-0 kKpPOn'})
@@ -15,7 +20,7 @@ def write_cmc_top():
     top_market_cap = 0
     for names, values in zip(list_name, list_values):
         name = names.text.strip()
-        value = float(values.text.replace(',', '').replace('$', '').replace('B', '').replace('T', ''))
+        value = float(values.text.replace(',', '').replace('$', '').replace('B', '').replace('T', '').replace('M', ''))
         top_market_cap += value
         market_percentage = round(value / top_market_cap * 100, 2)
         data.append([name, value, market_percentage])
@@ -35,5 +40,4 @@ def write_to_csv(data):
 
     print(f"Data saved to {filename}")
 
-data = write_cmc_top()
 write_to_csv(data)
